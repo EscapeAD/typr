d40b9769291ba52bef3dbbbbd78010391b24c201
Adam Tse <mr.adam.tse@gmail.com>
2016-10-20 19:10:29 -0400

    changed verifcation to a different system

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,26 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :uuid
+
+  def connect
+    self.uuid = SecureRandom.uuid
+  end
+
+    # identified_by :current_user
+    #
+    # def connect
+    #   current_user = verified_user
+    # end
+    #
+    # protected
+    #
+    # def verified_user
+    #   if current_user == User.find_by(id: cookies.signed['user.id'])
+    #     current_user
+    #   else
+    #     reject_unauthorized_connection
+    #   end
+    # end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => "/cable"
   devise_for :users
   root 'games#index'
-  resources :games
+  # resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
